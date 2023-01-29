25200794a78d31049589590659d7c54e11780858
Ajay Sambhaji Deshmukh <78377310+ajstyle2410@users.noreply.github.com>
2023-01-29 15:49:14 +0530

    navbar with bootstrap
     navbar with bootsrap  without css and add the js
    closes #38

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
+
+
 console.log(window.innerWidth);
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     var header = document.querySelector("nav");
     header.classList.toggle("sticky", window.scrollY > 0);
-
+    header.classList.toggle("bg-danger", window.scrollY > 0)
 })
+
+
+
