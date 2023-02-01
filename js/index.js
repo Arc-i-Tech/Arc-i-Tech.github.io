b7d3a0b19ca627fe5acea33461a3d5a5faf48f21
Ajay Sambhaji Deshmukh <78377310+ajstyle2410@users.noreply.github.com>
2023-02-01 18:42:58 +0530

    navbar Page using boottrap
    navigation bar using bootstrap
    #38

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@
 console.log(window.innerWidth);
 window.addEventListener("scroll", function () {
     var header = document.querySelector("nav");
-    header.classList.toggle("sticky", window.scrollY > 0);
     header.classList.toggle("bg-danger", window.scrollY > 0)
 })
 
