64658076f15e5642eefdce87a5cbfd668fd60649
ajayg2808 <AjayG.2808@gmail.com>
2023-06-24 21:25:44 +0530

    Handling contact form submission in Servlet
    1. Contact us form submission to servlet
    2. POST form data from servlet to fabform
    3. Updated project for HTML file formatting.

--- src/main/java/com/ait/servlets/ContactServlet.java
+++ src/main/java/com/ait/servlets/ContactServlet.java
@@ -1,6 +1,15 @@
 package com.ait.servlets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -34,7 +43,36 @@ public class ContactServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doGet(request, response);
+
+		StringBuilder theQueryString = new StringBuilder();
+		Map<String, String[]> map = request.getParameterMap();
+		map.forEach((key, value) -> {
+			theQueryString.append(key + "=" + Arrays.asList(value) + "&");
+		});
+
+		theQueryString.setLength(theQueryString.length() - 1);
+
+		URL url = new URL("https://fabform.io/f/MK2_1DD");
+		HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+		httpURLConnection.setRequestMethod("POST");
+		httpURLConnection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+		// For POST only - START
+		httpURLConnection.setDoOutput(true);
+		OutputStream os = httpURLConnection.getOutputStream();
+		os.write(theQueryString.toString().replace("[", "").replace("]", "").getBytes());
+		os.flush();
+		os.close();
+		// For POST only - END
+
+		int responseCode = httpURLConnection.getResponseCode();
+
+		if (201 == HttpURLConnection.HTTP_OK) {
+			response.sendRedirect("ThankYou");
+		} else {
+			this.doGet(request, response);
+		}
+
 	}
 
 }
