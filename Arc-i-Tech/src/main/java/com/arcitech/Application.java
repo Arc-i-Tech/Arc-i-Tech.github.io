ab87172fe08cd2750b8026aadd01a81d1517607e
ajayg2808 <AjayG.2808@gmail.com>
2024-10-09 00:08:32 +0530

    Created Springboot rest project
    Empty Springboot REST API project.

---
+++ Arc-i-Tech/src/main/java/com/arcitech/Application.java
@@ -0,0 +1,15 @@
+package com.arcitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
