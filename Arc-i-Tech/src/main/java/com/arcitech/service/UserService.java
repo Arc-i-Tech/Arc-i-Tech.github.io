b07d0cf0d4947e5f79bad57006db5e6099fcce24
Ajay Sambhaji Deshmukh <78377310+ajstyle2410@users.noreply.github.com>
2024-11-19 11:23:57 +0530

    profile delete request
     user profile delete by user id

---
+++ Arc-i-Tech/src/main/java/com/arcitech/service/UserService.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2024, Arc-i-Tech. All rights reserved.
+ * visit www.arc-i-tech.in
+ *
+ */
+package com.arcitech.service;
+
+/**
+ * @author AJ
+ * 
+ */
+public interface UserService {
+
+	  public   String deleteUserProfile(long id);
+	
+}
---
+++ Arc-i-Tech/src/main/java/com/arcitech/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2024, Arc-i-Tech. All rights reserved.
+ * visit www.arc-i-tech.in
+ *
+ */
+package com.arcitech.service;
+
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author AJ
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+  private  UserRepository userRepository;
+	
+	
+
+	  private static final Logger LOGGER = Logger.getLogger(UserServiceImpl.class);
+
+  @Override
+  public String deleteUserProfile(long id) {
+    LOGGER.info("Deleting user with ID: " + id);
+    try {
+      userRepository.deleteById(id);
+      LOGGER.info("User ID " + id + " deleted successfully.");
+      return "User ID " + id + " deleted successfully.";
+    } catch (Exception e) {
+      LOGGER.error("Error occurred while deleting user ID " + id, e);
+      return "Error deleting user ID " + id + ". Please try again later.";
+    }
+  }
+}
