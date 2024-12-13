ae401b03752e720aed47715a59dd8556726fbf6a
Ajay Sambhaji Deshmukh <78377310+ajstyle2410@users.noreply.github.com>
2024-12-13 11:09:37 +0530

    update  validation project
    remove the front end files

--- Frontend/Arc-i-Tech/src/app/app.routes.ts
+++ Frontend/Arc-i-Tech/src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './Auth/login/login.component';
-import { DashboardComponent } from './Auth/dashboard/dashboard.component';
-
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  {path:'dashboard',component:DashboardComponent}
+ 
 ];
 
 @NgModule({
