3878dd411b080e92fee176bb16b6a4d225b90ca3
Priya Vijay Pal <priyapal20009696@gmail.com>
2024-12-17 23:27:01 +0530

    Update UserRegistrationController.java

--- Arc-i-Tech/src/main/java/com/arcitech/controllers/UserRegistrationController.java
+++ Arc-i-Tech/src/main/java/com/arcitech/controllers/UserRegistrationController.java
@@ -26,18 +26,23 @@ import com.arcitech.service.UserService;
  * 
  */
 @Controller
+@RequestMapping("api/users")
 public class UserRegistrationController {
 	
 	@Autowired
 	private UserService userService;
 	
+	
 	@GetMapping("findByUserId/{id}")
 	public ResponseEntity<User> getUserById(@PathVariable Long id){
 		User user = userService.getUserById(id);
-		if(user==null){
+		if(user==null)
+		{
 			return new ResponseEntity<>(user, HttpStatus.OK);
+			
 		}
-		else{
+		else
+		{
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
