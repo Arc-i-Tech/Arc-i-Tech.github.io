9578c6533801c363ed1b254cc9bf8f56b5cc7f16
Priya Vijay Pal <priyapal20009696@gmail.com>
2024-12-17 22:46:50 +0530

    Update UserRegistrationController.java

--- Arc-i-Tech/src/main/java/com/arcitech/controllers/UserRegistrationController.java
+++ Arc-i-Tech/src/main/java/com/arcitech/controllers/UserRegistrationController.java
@@ -41,7 +41,6 @@ public class UserRegistrationController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
-
 	@PostMapping("/adduser")
 	public ResponseEntity<String> createUser(@RequestBody User user) {
 	    String response = userService.addUser(user);
