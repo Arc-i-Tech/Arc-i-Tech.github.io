e6a28b41ea199152fbecd8a670fd36de5c9614fd
Priya Vijay Pal <priyapal20009696@gmail.com>
2024-12-17 15:58:22 +0530

    RandomPasswordGenerator.java
    Changes have been made in the Random Password Generator.

---
+++ Arc-i-Tech/src/main/java/com/arcitech/helper/RandomPasswordGenerator.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2024, Arc-i-Tech. All rights reserved.
+ * visit www.arc-i-tech.in
+ *
+ */
+package com.arcitech.helper;
+
+import java.security.SecureRandom;
+
+/**
+ * @author Priya
+ * 
+ */
+public class RandomPasswordGenerator {
+	
+	private RandomPasswordGenerator() {
+		throw new UnsupportedOperationException("Password Generation");
+	}
+	public static String getAlphaNumericString(int n) 
+	{ 
+		String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz"; 
+		SecureRandom random = new SecureRandom();
+		
+		// create StringBuffer size of AlphaNumericString 
+		StringBuilder sb = new StringBuilder(n); 
+
+		for (int i = 0; i < n; i++) { 
+			// generate a random number between 
+			// 0 to AlphaNumericString variable length 
+			int randomIndex = random.nextInt(AlphaNumericString.length());
+	           char randomChar = AlphaNumericString.charAt(randomIndex);
+	           sb.append(randomChar);
+		} 
+		return sb.toString(); 
+	} 
+}
+
+
