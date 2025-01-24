cffedb75bfb3cb5619942f35ccbbb646ccda2342
Ajay Sambhaji Deshmukh <78377310+ajstyle2410@users.noreply.github.com>
2025-01-24 20:04:15 +0530

    create the dashboard
    create the dashboard using  angular
    angular inside implement the html and CSS add theme.css file

--- Frontend/Arc-i-Tech/src/app/app.routes.ts
+++ Frontend/Arc-i-Tech/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { TestUserComponent } from './dashboard/test-user/test-user.component';
+import { ServicesComponent } from './dashboard/services/services.component';
+import { UserFeedbackComponent } from './dashboard/user-feedback/user-feedback.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: ' ',redirectTo: '/test-user',pathMatch: 'full'
+  },
+  { path : 'test-user', component:TestUserComponent},
+  {
+    path: 'user-service',component:ServicesComponent
+  },
+  {
+    path: 'user-feedback',component:UserFeedbackComponent
+  }
+
+];
