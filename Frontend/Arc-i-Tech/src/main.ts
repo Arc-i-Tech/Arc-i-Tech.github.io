230cef3f41ba7c434b999fe04f17bd9bc878a6e0
Ajay Sambhaji Deshmukh <78377310+ajstyle2410@users.noreply.github.com>
2025-05-30 15:08:31 +0530

    update the angular file
    update the registration files   add the notifications

--- Frontend/Arc-i-Tech/src/main.ts
+++ Frontend/Arc-i-Tech/src/main.ts
@@ -1,8 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { AppComponent }          from './app/app.component';
+
+import { provideHttpClient }     from '@angular/common/http';
+import { provideToastr }            from 'ngx-toastr';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
-})
-.catch(err => console.error(err));
+  providers: [
+    provideHttpClient(),
+    provideToastr({
+      positionClass: 'toast-top-right',
+      timeOut: 3000,
+      closeButton: true,
+      progressBar: true,
+    }),
+  ],
+});
